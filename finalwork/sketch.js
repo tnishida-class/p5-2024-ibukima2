f946818f1799657fe367c7e62ef15164eb68ce65
ibukima2 <yibuki002@yahoo.ne.jp>
2024-11-25 16:05:23 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,94 @@
 // 最終課題を制作しよう
 
-function setup(){
+let squares = []; // 四角形の情報を保存する配列
+let size; // 四角形のサイズ
+const colors = []; // 色の配列
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
+  size = width / 16; // 四角形のサイズを計算
+
+  // colors 配列に色を定義
+  colors.push(color(0, 255, 0));  // green
+  colors.push(color(255, 0, 0));  // red
+  colors.push(color(0, 0, 255));  // blue
+  colors.push(color(255, 255, 0)); // yellow
+  colors.push(color(255));        // white
+
+  initSquares(); // 四角形の初期化
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  background(255); // 背景をリセット
+
+  // 四角形を描画
+  for (let square of squares) {
+    fill(colors[square.colorIndex]);
+    rect(square.x, square.y, square.size, square.size);
+  }
+
+  balloon("リセット", size*2, size*14.5)
+  text("クリックで色を変えられます", size*2, size + textAscent() + textDescent());
 }
 
-function windowResized(){
+function initSquares() {
+  // 四角形の情報を配列に保存
+  squares = [];
+  for (let i = 2; i < 14; i++) {
+    for (let j = 2; j < 14; j++) {
+      let x = size * i;
+      let y = size * j;
+      squares.push({
+        x: x, 
+        y: y, 
+        size: size, 
+        colorIndex: 0
+      });
+    }
+  }
+}
+
+function mousePressed() {
+  // マウスクリック位置を判定し、色を変更
+  for (let square of squares) {
+    if (
+      mouseX > square.x &&
+      mouseX < square.x + square.size &&
+      mouseY > square.y &&
+      mouseY < square.y + square.size
+    ) {
+      // 次の色に変更
+      square.colorIndex = (square.colorIndex + 1) % colors.length;
+      break; // 一つの四角形だけ変更   
+  }
+  }
+
+  let w = textWidth("リセット");
+  let h = textAscent() + textDescent();
+  let p = 2;
+  if(
+    mouseX > size*2 &&
+    mouseX < size*2 + w + p * 2 &&
+    mouseY > size*14.5 &&
+    mouseY < size*14.5 + h + p * 2
+  ) {
+   initSquares();
+   }
+}
+
+
+function balloon(t, x, y){
+  let w = textWidth(t);
+  let h = textAscent() + textDescent();
+  let p = 2;
+  fill(255);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(0)
+  text(t, x, y + h);
+}
+
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  size = width / 16; // サイズを再計算
+  initSquares(); // 四角形を再生成
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,10 +2,25 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
+  let csize = size - 5
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      let x = size * i;
+      let y = size * j;
+      if((i + j)%2 ==1){
+        fill(122);
+      rect(x,y,size,size);
+      }
+      if(((i + j)%2 == 1) && (j < 3)){
+        fill(255,0,0)
+        ellipse(x + size/2 , y + size/2 ,csize)
+      }
+      if(((i + j)%2 == 1) && (j > 4)){
+        fill(0)
+        ellipse(x + size/2 , y + size/2 ,csize)
     }
   }
+}
 }
